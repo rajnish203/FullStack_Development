f8d62c3d58b945950e3e7402fe6adfb7a812c36a
rajnish203 <rajnishdivakar618@gmail.com>
2023-07-03 17:30:52 +0530

    new version

---
+++ Backend/Node/Contact_List/index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+
+const path=require('path');
+
+const port=8000;
+
+const app=express();
+
+app.set('view engine', 'ejs');  
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/', function(req, res){
+   // console.log(__dirname);
+   // res.send('<h1>Cool, it is running ! or is it ?</h1>')
+
+   return res.render('index')
+})
+
+
+
+ app.listen(port, function(err){
+    if(err){ console.log('Error in runnig the server', err);}
+
+    console.log('Yup! My Express server is runnig on port', port);
+ })
